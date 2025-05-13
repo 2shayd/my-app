5f0e3b4389cc9fb832a9704de28282c41efddc77
Shay <132092823+2shayd@users.noreply.github.com>
2025-05-12 21:03:21 -0700

    zoom call with prof, his emulator is a white screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,45 +1,37 @@
-// import { SplashScreen, Stack } from "expo-router";
-// import { createContext, useEffect, useState } from "react";
-// import { GluestackUIProvider, ModeType } from "@/components/ui/gluestack-ui-provider";
-// import { useFonts } from "expo-font";
-// import { EntryProvider } from "@/components/ui/entry-context-provider";
-// // import { supabase } from "../app/utils/supabase";
+import { SplashScreen, Stack } from "expo-router";
+import { createContext, useEffect, useState } from "react";
+import { GluestackUIProvider, ModeType } from "@/components/ui/gluestack-ui-provider";
+import { useFonts } from "expo-font";
+import { EntryProvider } from "@/components/ui/entry-context-provider";
+// import { supabase } from "../app/utils/supabase";
 
 
-// type ThemeContextType = {
-//     colorMode?: ModeType;
-//     toggleColorMode?: () => void;
-// };
+type ThemeContextType = {
+    colorMode?: ModeType;
+    toggleColorMode?: () => void;
+};
 
-// export const ThemeContext = createContext<ThemeContextType>({
-//     colorMode: "light",
-//     toggleColorMode: () => {},
-// });
+export const ThemeContext = createContext<ThemeContextType>({
+    colorMode: "light",
+    toggleColorMode: () => {},
+});
 
-// export default function RootLayout() {
-//     const [colorMode, setColorMode] = useState<ModeType>("light");
+export default function RootLayout() {
+    const [colorMode, setColorMode] = useState<ModeType>("light");
 
-//     const [loaded] = useFonts({
-//         SpaceMono: require("@/assets/fonts/SpaceMono-Regular.ttf"),
-//     });
+    const [loaded] = useFonts({
+        SpaceMono: require("@/assets/fonts/SpaceMono-Regular.ttf"),
+    });
 
-//     useEffect(() => {
-//         if (loaded) {
-//             SplashScreen.hideAsync();
-//         }
-//     }, [loaded]);
-
-//         if (!loaded) {
-//             return null;
-//         }
-
-//         const toggleColorMode = async () => {
-//             setColorMode((prev) => (prev === "light" ? "dark" : "light"));
-//         };
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
 
         //handle initial supabase auth
     // need to be completed --->
-//         useEffect(() => {
+        useEffect(() => {
 //             if (isAuthenticated) {
 //                 return;
 //             }
@@ -67,20 +59,27 @@
 //             }
 //         }
     
-//         autoSign();
-//     })
-//     // <--- need to be completed
+        // autoSign();
+    }, [])
+    // <--- need to be completed
 
-//         return (
-//             <GluestackUIProvider mode={colorMode}>
-//                 <ThemeContext.Provider value={{ colorMode, toggleColorMode }}>
-//                     <EntryProvider>
-//                         <Stack>
-//                             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-//                             <Stack.Screen name="+not-found" options={{ headerShown: false }} />
-//                         </Stack>
-// 0                    </EntryProvider>
-//                 </ThemeContext.Provider>
-//             </GluestackUIProvider>
-//         );
-    // }
+    if (!loaded) {
+        return null;
+    }
+    const toggleColorMode = async () => {
+        setColorMode((prev) => (prev === "light" ? "dark" : "light"));
+    };
+   
+        return (
+            <GluestackUIProvider mode={colorMode}>
+                <ThemeContext.Provider value={{ colorMode, toggleColorMode }}>
+                    <EntryProvider>
+                        <Stack>
+                            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                            <Stack.Screen name="+not-found" options={{ headerShown: false }} />
+                        </Stack>
+0                    </EntryProvider>
+                </ThemeContext.Provider>
+            </GluestackUIProvider>
+        );
+    }
